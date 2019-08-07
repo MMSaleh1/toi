a510994f8a4c5960dd216bf0daadbbdfd80bf3a2
DESKTOP-KKV1KKP\Mohammad Magdy <mohammed.magdy.ali.96@gmail.com>
2019-08-07 12:16:02 +0200

    new ups

--- src/pages/map-modal/map-modal.ts
+++ src/pages/map-modal/map-modal.ts
@@ -52,8 +52,8 @@ export class MapModalPage {
   }
 
   ionViewDidLoad() {
-    // this.checkIfLocationEnablenOrNot();
-    this.InitMap();
+    this.checkIfLocationEnablenOrNot();
+    // this.InitMap();
   }
 
   ionViewDidEnter() {
